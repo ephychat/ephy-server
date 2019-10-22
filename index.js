770e9eebd4c368dab4024a1ca4f86dbbe90b28f7
devmoses <moses.thipparthi@yahoo.com>
2019-10-22 15:33:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express =  require('express');
+      helmet =  require('helmet');
+      dotnev = require('dotenv').config();
+      app =  express();
+
+app.use(helmet());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(`Hello from ${process.env.SERVER_NAME} ${process.env.SERVER_VERSION}`);
+});
+
+const listener = app.listen(process.env.SERVER_PORT, () => {
+    console.log(`${process.env.SERVER_NAME} ${process.env.SERVER_VERSION} running on ${listener.address().port}`)
+});
