7c16a0ab331287de3f9728253c7ab6a7e51d845e
devmoses <moses.thipparthi@yahoo.com>
2019-10-23 13:28:02 +0530

    Add eslint config file and update package.json to use index.json as main

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'indent': [
+      'error',
+      2
+    ],
+    'max-lines-per-function': [
+      'error', 
+      {
+        'max': 20,
+        'skipBlankLines': true,
+        'skipComments': true
+      },
+    ],
+    "yoda": [
+      "error",
+    ],
+    "no-shadow": [
+      "error"
+    ],
+  },
+};
